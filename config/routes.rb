783993137cf8fe2f0db0e4abdb5f15eb119ade61
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-04 06:29:08 +0000

    [Update] Adminルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admins/sessions'
+  }
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
