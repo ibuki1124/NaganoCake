18d954bf15f4ff41f7fa1a31de8b337640e25fb7
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-05 19:33:06 +0000

    [Add] public/homesコントローラ・ビュー・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
     sessions: 'public/sessions',
     registrations: 'public/registrations'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 会員側
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => "homes#about", as: "about"
+  end
 end
