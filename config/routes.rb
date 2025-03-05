3428982295d60a135cddfbb823a8740586d73ec4
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-05 11:09:38 +0000

    [Add] devise：admin/publicビュー・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  # 管理者用
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+  # 顧客用
+  devise_for :customers, skip: [:passwords], controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
