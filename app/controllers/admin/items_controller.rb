b2a95b7f624ffeb7014aeae7d5d9c2e72ac2f18f
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-10 11:33:22 +0000

    [Add] 商品新規登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,11 +3,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :genre_id, :is_active)
   end
 end
