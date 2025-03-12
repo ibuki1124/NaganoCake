06738904f2b60410002e829fea782b53b3267125
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-12 06:44:22 +0000

    [Add] public：カート削除

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,6 +10,18 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
 
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    cart_items = current_customer.cart_items
+    cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
