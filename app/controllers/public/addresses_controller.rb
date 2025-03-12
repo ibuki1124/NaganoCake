01af6f96490b6d8533f8885581cd7881fcc8f9b6
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-12 04:19:09 +0000

    [Add] public：配送先新規登録・一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,21 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    address.save
+    redirect_to addresses_path
   end
 
   def edit
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
