abc6cd535bf4d48185c58d5b70635f7fccdd3bae
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-12 11:41:19 +0000

    [Update] public：カートに既に同商品が存在した場合の処理

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,10 +4,22 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.save
-    redirect_to cart_items_path
+    item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    if item.present?
+      cart_item_amount = item.amount + params[:cart_item][:amount].to_i
+      if cart_item_amount > 10
+        item.amount = 10
+      else
+        item.amount = cart_item_amount
+      end
+      item.save!
+      redirect_to cart_items_path
+    else
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.customer_id = current_customer.id
+      cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def update
