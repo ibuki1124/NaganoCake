36c59ef7dae3806902836a1985f7a54a3a78a32b
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-17 04:57:47 +0000

    [Update] Orderモデル・enum・日本語化

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting: 0, confirm: 1, making: 2, preparing: 3, shipped: 4 }
 end
