9d1da38f504f6089f76e4ed5882fd882400194b2
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-22 09:28:08 +0000

    [Add] admin：注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_detail = @order.order_details
+    @total_price = 0
+    @order_detail.each do |order_detail|
+      @total_price += order_detail.item.with_tax_price * order_detail.amount
+    end
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
