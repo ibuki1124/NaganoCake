4b6836b093e0924423b8badd757217f2154667cb
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-25 02:49:39 +0000

    [Update] ステータス連動更新

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,12 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
+    if @order_detail.making_status == "in_progress"
+      @order_detail.order.update(status: 2)
+    end
+    if @order_detail.order.order_details.all? { |order_detail| order_detail.making_status == "done" }
+      @order_detail.order.update(status: 3)
+    end
     redirect_to admin_order_path(@order_detail.order)
   end
 
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,6 +11,11 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
+    if @order.status == "confirm"
+      @order.order_details.each do |order_detail|
+        order_detail.update(making_status: 1)
+      end
+    end
     redirect_to admin_order_path(@order)
   end
 
