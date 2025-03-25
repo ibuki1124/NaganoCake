eaf86c49b7bf103c00af3d4027939b520ed40a16
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-25 05:08:38 +0000

    [Add] 商品検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(8)
+    if params[:search].present?
+      @items = Item.where('name LIKE ?', "%#{params[:search]}%").page(params[:page]).per(8)
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
   end
 
   def show
