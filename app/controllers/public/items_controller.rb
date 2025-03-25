a18625e3a4ca6bfe11554a554c6394ae26f2cf9f
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-25 05:52:22 +0000

    [Add] public：ジャンル検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.page(params[:genre_page]).per(6)
     if params[:search].present?
       @items = Item.where('name LIKE ?', "%#{params[:search]}%").page(params[:page]).per(8)
+      @title = params[:search] + "の検索結果"
+    elsif params[:genre_name].present?
+      @items = Item.where(genre_id: Genre.find_by(name: params[:genre_name]).id).page(params[:page]).per(8)
+      @title = params[:genre_name] + "一覧"
     else
-      @items = Item.page(params[:page]).per(8)
+      @items = Item.page(params[:item_page]).per(8)
+      @title = "商品一覧"
     end
+    @total_count = @items.count
   end
 
   def show
