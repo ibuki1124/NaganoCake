849ecea16e79ca35e06ad9657b345ac8413c795e
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-03-27 04:10:30 +0000

    [Add]top・aboutページ

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @genres = Genre.page(params[:genre_page]).per(6)
+    @items = Item.where(is_active: true).order(created_at: :desc).limit(4)
   end
 
   def about
