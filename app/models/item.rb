148654a31eec7683a32e911828d69a1bc8b0a250
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2025-04-28 11:53:09 +0000

    [Add] 各モデルバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,11 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
   def get_item_image(width, height)
     unless image.attached?
       file_path = Rails.root.join("app/assets/images/no_image.jpg")
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :payment_method, presence: true
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting: 0, confirm: 1, making: 2, preparing: 3, shipped: 4 }
